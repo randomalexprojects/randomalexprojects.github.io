905fba39fdde54f041298242c8831502251cf51f
ASeeto <seeto.alexander@gmail.com>
2019-09-18 00:06:17 -0400

    Initial single page navigation

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {NAVIGATION} from './constants/navigation'
+
 class App extends Component {
+  constructor () {
+    super()
+    this.updatePage = this.updatePage.bind(this)
+    this.state = {
+      page: NAVIGATION.HOME
+    }
+  }
+
+  updatePage (e) {
+    this.setState({page: e.target.id})
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-header">
+          <div className='app-header-nav'>
+            <p id={NAVIGATION.HOME} onClick={this.updatePage}>Home</p>
+            <p id={NAVIGATION.ABOUT} onClick={this.updatePage}>About Us</p>
+            <p id={NAVIGATION.DETAILS} onClick={this.updatePage}>Wedding Details</p>
+            <p id={NAVIGATION.ACCOMMODATIONS} onClick={this.updatePage}>Accommodations</p>
+            <p id={NAVIGATION.WEDDING} onClick={this.updatePage}>Wedding Party</p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-content">
+          {this.state.page === NAVIGATION.HOME && (
+            <div className='page'>HOME</div>
+          )}
+          {this.state.page === NAVIGATION.ABOUT && (
+            <div className='page'>ABOUT</div>
+          )}
+          {this.state.page === NAVIGATION.DETAILS && (
+            <div className='page'>DETAILS</div>
+          )}
+          {this.state.page === NAVIGATION.ACCOMMODATIONS && (
+            <div className='page'>ACCOMMODATIONS</div>
+          )}
+          {this.state.page === NAVIGATION.WEDDING && (
+            <div className='page'>WEDDING</div>
+          )}
+        </div>
       </div>
     );
   }
